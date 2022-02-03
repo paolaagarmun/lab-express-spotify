9ac8189e54a64a7be89fed8af1065f4fab70e3fd
paolaagarmun <paola.a.garciamunoz@gmail.com>
2022-02-02 23:55:05 -0500

    worked on iteration 2 and 3

--- app.js
+++ app.js
@@ -28,5 +28,16 @@ const spotifyApi = new SpotifyWebApi({
 // Our routes go here:
 app.get("/", (req, res, next) => res.render("index"));
 
+app.get("/artist-search", (req,res) => {
+    spotifyApi
+  .searchArtists(req.query.artist)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    res.render("artist-search-results", {artistInfo: data.body.artists.items});
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
